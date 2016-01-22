5c6e4059651ef818169c3ec06b1d0fbb569b7cae
Niicck <nickivons@yahoo.com>
2016-01-21 20:33:12 -0800

    (feature) create formula to calculate distance from lat/long in meters

--- helpers/index.js
+++ helpers/index.js
@@ -2,20 +2,18 @@ var math = require('mathjs');
 
 var haversine = function(lat1, long1, lat2, long2){
 
-  var dlat = toRadians(lat1 - lat2);
-  var dlong = toRadians(long1 - long2);
+  lat1 = toRadians(lat1);
+  long1 = toRadians(long1);
+  lat2 = toRadians(lat2);
+  long2 = toRadians(long2);
+
+  var dlat = lat1 - lat2;
+  var dlong = long1 - long2;
 
   var angle = math.pow(math.sin(dlat/2),2) + math.cos(lat1) * math.cos(lat2) * math.pow(math.sin(dlong/2),2);
-  // if (angle < 0) {
-  //   throw "Angle is too small" + angle;
-  //   // console.log("Angle is too small", angle);
-  //   // return false
-  // } else {
-  console.log("What's the angle", angle);
   var circle = 2 * math.atan2(math.sqrt(angle), math.sqrt(1-angle));
   var distance = 6367000 * circle;
   return distance;
-  // }
 }
 
 var toRadians = function(deg) {
@@ -23,6 +21,7 @@ var toRadians = function(deg) {
 }
 
 exports.withinRange = function(lat1, long1, lat2, long2, m) {
+<<<<<<< HEAD
   var dist = haversine(lat1,long1,lat2,long2)
   if (dist) {
     return dist <= m;
@@ -30,3 +29,17 @@ exports.withinRange = function(lat1, long1, lat2, long2, m) {
     return false;
   };
 }
+=======
+  if (haversine(lat1,long1,lat2,long2) <= m) {
+    return true;
+  }
+}
+
+var lat1 =48.859065
+var long1 = 2.294518
+
+var lat2 = 48.857470
+var long2 = 2.294497
+
+console.log("Distance is", haversine(lat1,long1,lat2,long2));
+>>>>>>> (feature) create formula to calculate distance from lat/long in meters
