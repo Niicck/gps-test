6d59043436111d1b7eff1f6a9b93103a684a6ca7
Niicck <nickivons@yahoo.com>
2016-01-22 15:13:43 -0800

    (test) make smaller target radius

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ io.on('connection', function(socket){
       lat: 34.0192316,
       long: -118.4943091,
       type: "flag",
-      range: 100,
+      range: 20,
     }
 
     if (withinRange(data.lat, data.long, item.lat, item.long, item.range)){
