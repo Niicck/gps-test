a5186d1e64e7c30341e2fc18100c7a02074550a0
luisgamero <gamero.luis.enrique@gmail.com>
2016-01-21 20:03:29 -0800

    implement collision detection (test)

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var withinRange = require('helpers/index.js').withinRange;
 var usernames = [];
+var target = {
+  lat: 34.0192699,
+  long: -118.4943795
+};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -16,14 +20,19 @@ io.on('connection', function(socket){
     console.log('usernames sent to clients: ', usernames);
   });
 
-  socket.on('chat-message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat-broadcast', msg);
+  socket.on('chat-message', function(data){
+    console.log('message: ' + data);
+
+    if (Math.abs(data.lat - target.lat) < 0.000025 && Math.abs(data.long - target.long) < 0.000025) {
+      io.emit('chat-broadcast', data.username + " COLLISION WITH TARGET!");
+    } else {
+      io.emit('chat-broadcast', data.username + " position: " + data.lat + data.long);
+    }
   });
 
   socket.on('disconnect', function(){
     console.log('user disconnected :(');
-  })
+  });
 });
 
 var port = process.env.PORT || 5000;
